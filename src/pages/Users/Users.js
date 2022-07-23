223f80d46d02a7619f05a09fa65b058702097028
shaban6 <ahshaban@binery.co>
2022-07-23 18:18:06 +0200

    shaban first edit

--- src/pages/Users/Users.js
+++ src/pages/Users/Users.js
@@ -21,37 +21,40 @@ const Users = () => {
         dispatch(deleteUser(id));
     };
 
-  const { users, loading, error } = useSelector((state) => state.allUsers);
-  const {
-    room: deletedRoom,
-    loading: deleteLoading,
-    success: deleteSuccess,
-    error: deleteError,
-  } = useSelector((state) => state.deleteUser);
-  useEffect(() => {
-    dispatch(getAllUsers(7, 7));
-  }, [dispatch, currentPage, limit]);
-  console.log(users?.rows);
-  const columns = ["ID", "User Name", "Email", "Edit", "Delete"];
+    const { users, loading, error } = useSelector((state) => state.allUsers);
+    const {
+        room: deletedRoom,
+        loading: deleteLoading,
+        success: deleteSuccess,
+        error: deleteError,
+    } = useSelector((state) => state.deleteUser);
+    useEffect(() => {
+        dispatch(getAllUsers(7, 7));
+    }, [dispatch, currentPage, limit]);
+    console.log(users?.rows);
+    const columns = ['ID', 'User Name', 'Email', 'Edit', 'Delete'];
 
     const data = [];
 
-  if (users?.rows.length !== 0) {
-    for (let u in users?.rows) {
-      data.push({
-        id: users?.rows[u].id,
-        ID: users?.rows[u].id || "no data",
-        "User Name": users?.rows[u].username || "no data",
-        Email: users?.rows[u].email || "no data",
-        Edit: (
-          <EditButton updateHandler={() => updateHandler(users?.rows[u].id)} />
-        ),
-        Delete: (
-          <DeleteButton
-            deleteHandler={() => deleteHandler(users?.rows[u].id)}
-          />
-        ),
-      });
+    if (users?.rows.length !== 0) {
+        for (let u in users?.rows) {
+            data.push({
+                id: users?.rows[u].id,
+                ID: users?.rows[u].id || 'no data',
+                'User Name': users?.rows[u].username || 'no data',
+                Email: users?.rows[u].email || 'no data',
+                Edit: (
+                    <EditButton
+                        updateHandler={() => updateHandler(users?.rows[u].id)}
+                    />
+                ),
+                Delete: (
+                    <DeleteButton
+                        deleteHandler={() => deleteHandler(users?.rows[u].id)}
+                    />
+                ),
+            });
+        }
     }
 
     const pageChangeHandler = (e, page) => {
@@ -59,35 +62,35 @@ const Users = () => {
         setCurrentPage(page);
     };
 
-  return (
-    <div className={styles.sessionsContainer}>
-      {error &&
-        (Array.isArray(error) ? (
-          error.map((e) => <Error>{e.msg}</Error>)
-        ) : (
-          <Error>{error}</Error>
-        ))}
-      {deleteError && <Error>{deleteError}</Error>}
-      {deleteSuccess && <Success>User deleted successfully</Success>}
-      <div className={styles.sessionsTable}>
-        {loading ? (
-          <p>loading...</p>
-        ) : (
-          <Table
-            data={data}
-            columns={columns}
-            tableName="Users"
-            showDetails={false}
-            addNewButton={true}
-            pageChangeHandler={pageChangeHandler}
-            currentPage={currentPage}
-            dataCount={users?.count}
-            limit={limit}
-          />
-        )}
-      </div>
-    </div>
-  );
+    return (
+        <div className={styles.sessionsContainer}>
+            {error &&
+                (Array.isArray(error) ? (
+                    error.map((e) => <Error>{e.msg}</Error>)
+                ) : (
+                    <Error>{error}</Error>
+                ))}
+            {deleteError && <Error>{deleteError}</Error>}
+            {deleteSuccess && <Success>User deleted successfully</Success>}
+            <div className={styles.sessionsTable}>
+                {loading ? (
+                    <p>loading...</p>
+                ) : (
+                    <Table
+                        data={data}
+                        columns={columns}
+                        tableName='Users'
+                        showDetails={false}
+                        addNewButton={true}
+                        pageChangeHandler={pageChangeHandler}
+                        currentPage={currentPage}
+                        dataCount={users?.count}
+                        limit={limit}
+                    />
+                )}
+            </div>
+        </div>
+    );
 };
 
 export default Users;
