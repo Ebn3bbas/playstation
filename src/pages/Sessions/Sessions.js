88dab5274b4c20b8b546eb03a42a54a8c8792b03
Ebn3bbas <92022849+Ebn3bbas@users.noreply.github.com>
2022-07-24 18:39:12 +0200

    update

--- src/pages/Sessions/Sessions.js
+++ src/pages/Sessions/Sessions.js
@@ -1,207 +1,189 @@
-import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router';
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
 import {
-    deleteSession,
-    getAllEndedSessions,
-    getAllNotEndedSessions,
-    getAllSessions,
-} from '../../actions/sessions';
-import DeleteButton from '../../components/Buttons/DeleteButton/DeleteButton';
-import EditButton from '../../components/Buttons/EditButton/EditButton';
-import Table from '../../components/Table/Table';
-import { useDispatch, useSelector } from 'react-redux';
-import styles from './Sessions.module.css';
-import Error from '../../components/Error/Error';
-import Success from '../../components/Success/Success';
-import Timer from 'react-timer-wrapper';
-import Timecode from 'react-timecode';
-import { useTimer } from 'use-timer';
-import PrimaryButton from '../../components/Buttons/PrimaryButton/PrimaryButton';
+  deleteSession,
+  getAllEndedSessions,
+  getAllNotEndedSessions,
+  getAllSessions,
+} from "../../actions/sessions";
+import DeleteButton from "../../components/Buttons/DeleteButton/DeleteButton";
+import EditButton from "../../components/Buttons/EditButton/EditButton";
+import Table from "../../components/Table/Table";
+import { useDispatch, useSelector } from "react-redux";
+import styles from "./Sessions.module.css";
+import Error from "../../components/Error/Error";
+import Success from "../../components/Success/Success";
+import PrimaryButton from "../../components/Buttons/PrimaryButton/PrimaryButton";
 
 const Sessions = () => {
-    let limit = 10;
-    const [currentPage, setCurrentPage] = useState(1);
-    const [currentEndedPage, setCurrentEndedPage] = useState(1);
+  let limit = 10;
+  const [currentPage, setCurrentPage] = useState(1);
+  const [currentEndedPage, setCurrentEndedPage] = useState(1);
 
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-    const updateHandler = (sessionId) => {
-        navigate(`/sessions-update-form/${sessionId}`);
-    };
+  const updateHandler = (sessionId) => {
+    navigate(`/sessions-update-form/${sessionId}`);
+  };
 
-    const deleteHandler = (sessionId) => {
-        dispatch(deleteSession(sessionId));
-    };
+  const deleteHandler = (sessionId) => {
+    dispatch(deleteSession(sessionId));
+  };
 
-    const { endedSessions, loading, error } = useSelector(
-        (state) => state.allEndedSessions
-    );
+  const { endedSessions, loading, error } = useSelector(
+    (state) => state.allEndedSessions
+  );
 
-    const {
-        notEndedSessions,
-        loading: notEndedLoading,
-        error: notEndeddError,
-    } = useSelector((state) => state.allNotEndedSessions);
-    const {
-        session: deletedSession,
-        loading: deleteLoading,
-        success: deleteSuccess,
-        error: deleteError,
-    } = useSelector((state) => state.deleteSession);
+  const {
+    notEndedSessions,
+    loading: notEndedLoading,
+    error: notEndeddError,
+  } = useSelector((state) => state.allNotEndedSessions);
+  const {
+    session: deletedSession,
+    loading: deleteLoading,
+    success: deleteSuccess,
+    error: deleteError,
+  } = useSelector((state) => state.deleteSession);
 
-    useEffect(() => {
-        dispatch(getAllEndedSessions(currentEndedPage, limit));
-        dispatch(getAllNotEndedSessions(currentPage, limit));
-    }, [dispatch, currentPage, limit, deleteLoading, currentEndedPage]);
+  useEffect(() => {
+    dispatch(getAllEndedSessions(currentEndedPage, limit));
+    dispatch(getAllNotEndedSessions(currentPage, limit));
+  }, [dispatch, currentPage, limit, deleteLoading, currentEndedPage]);
 
-    const columns = [
-        'Id',
-        'PS',
-        'Type',
-        'Started At',
-        'add_orders',
-        'edit',
-        'delete',
-    ];
-    const Endedcolumns = [
-        'Id',
-        'PS',
-        'Type',
-        'Started At',
-        'Ended At',
-        'edit',
-        'delete',
-    ];
-    const data = [];
-    const endedData = [];
+  const columns = [
+    "Id",
+    "PS",
+    "Type",
+    "Started At",
+    "add_orders",
+    "edit",
+    "delete",
+  ];
+  const Endedcolumns = [
+    "Id",
+    "PS",
+    "Type",
+    "Started At",
+    "Ended At",
+    "edit",
+    "delete",
+  ];
+  const data = [];
+  const endedData = [];
 
-    if (endedSessions && endedSessions?.rows?.length !== 0) {
-        for (let r in endedSessions.rows) {
-            endedData.push({
-                id: endedSessions?.rows[r]?.id,
-                ID: endedSessions?.rows[r]?.id || 'no data',
-                PS: endedSessions?.rows[r]?.playstation?.title || 'no data',
-                type: endedSessions?.rows[r]?.type || 'no data',
-                started_at:
-                    endedSessions?.rows[r]?.started_at?.substr(11, 8) ||
-                    'no data',
-                ended_at:
-                    endedSessions?.rows[r]?.ended_at?.substr(11, 8) ||
-                    'not ended yet',
-                edit: (
-                    <EditButton
-                        updateHandler={() =>
-                            updateHandler(endedSessions?.rows[r]?.id)
-                        }
-                    />
-                ),
-                delete: (
-                    <DeleteButton
-                        deleteHandler={() =>
-                            deleteHandler(endedSessions?.rows[r]?.id)
-                        }
-                    />
-                ),
-            });
-        }
+  if (endedSessions && endedSessions?.rows?.length !== 0) {
+    for (let r in endedSessions.rows) {
+      endedData.push({
+        id: endedSessions?.rows[r]?.id,
+        ID: endedSessions?.rows[r]?.id || "no data",
+        PS: endedSessions?.rows[r]?.playstation?.title || "no data",
+        type: endedSessions?.rows[r]?.type || "no data",
+        started_at:
+          endedSessions?.rows[r]?.started_at?.substr(11, 8) || "no data",
+        ended_at:
+          endedSessions?.rows[r]?.ended_at?.substr(11, 8) || "not ended yet",
+        edit: (
+          <EditButton
+            updateHandler={() => updateHandler(endedSessions?.rows[r]?.id)}
+          />
+        ),
+        delete: (
+          <DeleteButton
+            deleteHandler={() => deleteHandler(endedSessions?.rows[r]?.id)}
+          />
+        ),
+      });
     }
+  }
 
-    if (notEndedSessions && notEndedSessions?.rows?.length !== 0) {
-        for (let r in notEndedSessions.rows) {
-            data.push({
-                id: notEndedSessions?.rows[r]?.id,
-                ID: notEndedSessions?.rows[r]?.id || 'no data',
-                PS: notEndedSessions?.rows[r]?.playstation?.title || 'no data',
-                type: notEndedSessions?.rows[r]?.type || 'no data',
-                started_at:
-                    notEndedSessions?.rows[r]?.started_at?.substr(11, 8) ||
-                    'no data',
+  if (notEndedSessions && notEndedSessions?.rows?.length !== 0) {
+    for (let r in notEndedSessions.rows) {
+      data.push({
+        id: notEndedSessions?.rows[r]?.id,
+        ID: notEndedSessions?.rows[r]?.id || "no data",
+        PS: notEndedSessions?.rows[r]?.playstation?.title || "no data",
+        type: notEndedSessions?.rows[r]?.type || "no data",
+        started_at:
+          notEndedSessions?.rows[r]?.started_at?.substr(11, 8) || "no data",
 
-                add_orders: (
-                    <PrimaryButton
-                        title='Add Orders'
-                        onClick={() =>
-                            navigate(
-                                `/sessions-add-orders/${notEndedSessions?.rows[r]?.id}`
-                            )
-                        }
-                    />
-                ),
-                edit: (
-                    <EditButton
-                        updateHandler={() =>
-                            updateHandler(notEndedSessions?.rows[r]?.id)
-                        }
-                    />
-                ),
-                delete: (
-                    <DeleteButton
-                        deleteHandler={() =>
-                            deleteHandler(notEndedSessions?.rows[r]?.id)
-                        }
-                    />
-                ),
-            });
-        }
+        add_orders: (
+          <PrimaryButton
+            title="Add Orders"
+            onClick={() =>
+              navigate(`/sessions-add-orders/${notEndedSessions?.rows[r]?.id}`)
+            }
+          />
+        ),
+        edit: (
+          <EditButton
+            updateHandler={() => updateHandler(notEndedSessions?.rows[r]?.id)}
+          />
+        ),
+        delete: (
+          <DeleteButton
+            deleteHandler={() => deleteHandler(notEndedSessions?.rows[r]?.id)}
+          />
+        ),
+      });
     }
+  }
 
-    console.log(endedSessions);
+  console.log(endedSessions);
 
-    const pageChangeHandler = (e, page) => {
-        e.preventDefault();
-        setCurrentPage(page);
-    };
+  const pageChangeHandler = (e, page) => {
+    e.preventDefault();
+    setCurrentPage(page);
+  };
 
-    const endedPageChangeHandler = (e, page) => {
-        e.preventDefault();
-        setCurrentEndedPage(page);
-    };
+  const endedPageChangeHandler = (e, page) => {
+    e.preventDefault();
+    setCurrentEndedPage(page);
+  };
 
-    return (
-        <div className={styles.RoomsContainer}>
-            <div className={styles.RoomsTable}>
-                {error &&
-                    (Array.isArray(error) ? (
-                        error.map((e) => <Error>{e.msg}</Error>)
-                    ) : (
-                        <Error>{error}</Error>
-                    ))}
-                {deleteError && <Error>{deleteError}</Error>}
-                {deleteSuccess && (
-                    <Success>Seesion deleted successfully</Success>
-                )}
-                {loading ? (
-                    <p>loading...</p>
-                ) : (
-                    <div>
-                        <Table
-                            data={data}
-                            columns={columns}
-                            tableName='Sessions'
-                            showDetails={true}
-                            addNewButton={true}
-                            pageChangeHandler={pageChangeHandler}
-                            currentPage={currentPage}
-                            dataCount={notEndedSessions?.count}
-                            limit={limit}
-                        />
-                        <Table
-                            data={endedData}
-                            columns={Endedcolumns}
-                            tableName='Sessions'
-                            showDetails={true}
-                            addNewButton={false}
-                            pageChangeHandler={endedPageChangeHandler}
-                            currentPage={currentEndedPage}
-                            dataCount={endedSessions?.count}
-                            limit={limit}
-                        />
-                    </div>
-                )}
-            </div>
-        </div>
-    );
+  return (
+    <div className={styles.RoomsContainer}>
+      <div className={styles.RoomsTable}>
+        {error &&
+          (Array.isArray(error) ? (
+            error.map((e) => <Error>{e.msg}</Error>)
+          ) : (
+            <Error>{error}</Error>
+          ))}
+        {deleteError && <Error>{deleteError}</Error>}
+        {deleteSuccess && <Success>Seesion deleted successfully</Success>}
+        {loading ? (
+          <p>loading...</p>
+        ) : (
+          <div>
+            <Table
+              data={data}
+              columns={columns}
+              tableName="Sessions"
+              showDetails={true}
+              addNewButton={true}
+              pageChangeHandler={pageChangeHandler}
+              currentPage={currentPage}
+              dataCount={notEndedSessions?.count}
+              limit={limit}
+            />
+            <Table
+              data={endedData}
+              columns={Endedcolumns}
+              tableName="Sessions"
+              showDetails={true}
+              addNewButton={false}
+              pageChangeHandler={endedPageChangeHandler}
+              currentPage={currentEndedPage}
+              dataCount={endedSessions?.count}
+              limit={limit}
+            />
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Sessions;
--- src/pages/Sessions/SessionsForm/SessionsForm.js
+++ src/pages/Sessions/SessionsForm/SessionsForm.js
@@ -1,128 +1,126 @@
-import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate, useParams } from 'react-router';
-import { getAllDevices } from '../../../actions/device';
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate, useParams } from "react-router";
+import { getAllDevices } from "../../../actions/device";
 import {
-    addSession,
-    getSession,
-    updateSession,
-} from '../../../actions/sessions';
-import BackButton from '../../../components/Buttons/BackButton/BackButton';
-import Error from '../../../components/Error/Error';
-import Form from '../../../components/Form/Form';
-import Input from '../../../components/Form/Input/Input';
-import Select from '../../../components/Form/Select/Select';
-import Success from '../../../components/Success/Success';
+  addSession,
+  getSession,
+  updateSession,
+} from "../../../actions/sessions";
+import BackButton from "../../../components/Buttons/BackButton/BackButton";
+import Error from "../../../components/Error/Error";
+import Form from "../../../components/Form/Form";
+import Input from "../../../components/Form/Input/Input";
+import Select from "../../../components/Form/Select/Select";
+import Success from "../../../components/Success/Success";
 import {
-    ADD_SESSION_RESET,
-    UPDATE_SESSION_RESET,
-} from '../../../constants/sessions';
-import styles from './SessionsForm.module.css';
+  ADD_SESSION_RESET,
+  UPDATE_SESSION_RESET,
+} from "../../../constants/sessions";
+import styles from "./SessionsForm.module.css";
 
 const initialState = {
-    hours: undefined,
-    type: undefined,
-    device: undefined,
+  hours: undefined,
+  type: undefined,
+  device: undefined,
 };
 
 const SessionsForm = ({ isNew }) => {
-    const dispatch = useDispatch();
-    const params = useParams();
-    const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const params = useParams();
+  const navigate = useNavigate();
 
-    const submitHandler = (e, form) => {
-        e.preventDefault();
-        const newForm = {
-            ...form,
-            device: devices?.find((d) => d.title === form.device),
-        };
-        if (isNew) {
-            dispatch(addSession(newForm));
-        } else {
-            console.log(newForm);
-            dispatch(
-                updateSession(params.id, sessionDetails.playstation.id, newForm)
-            );
-        }
+  const submitHandler = (e, form) => {
+    e.preventDefault();
+    const newForm = {
+      ...form,
+      device: devices?.find((d) => d.title === form.device),
     };
+    if (isNew) {
+      dispatch(addSession(newForm));
+    } else {
+      console.log(newForm);
+      dispatch(
+        updateSession(params.id, sessionDetails.playstation.id, newForm)
+      );
+    }
+  };
 
-    const { session: sessionDetails } = useSelector(
-        (state) => state.getSession
-    );
+  const { session: sessionDetails } = useSelector((state) => state.getSession);
 
-    const { session, loading, success, error } = useSelector((state) => {
-        if (isNew) {
-            return state.addSession;
-        } else {
-            return state.updateSession;
-        }
-    });
+  const { session, loading, success, error } = useSelector((state) => {
+    if (isNew) {
+      return state.addSession;
+    } else {
+      return state.updateSession;
+    }
+  });
 
-    const { devices } = useSelector((state) => state.allDevices);
+  const { devices } = useSelector((state) => state.allDevices);
+  console.log(devices);
+  useEffect(() => {
+    if (success) {
+      setTimeout(() => {
+        dispatch({
+          type: isNew ? ADD_SESSION_RESET : UPDATE_SESSION_RESET,
+        });
+        navigate("/sessions");
+      }, 600);
+    }
+  }, [success, navigate, dispatch, isNew]);
 
-    useEffect(() => {
-        if (success) {
-            setTimeout(() => {
-                dispatch({
-                    type: isNew ? ADD_SESSION_RESET : UPDATE_SESSION_RESET,
-                });
-                navigate('/sessions');
-            }, 600);
-        }
-    }, [success, navigate, dispatch, isNew]);
+  useEffect(() => {
+    dispatch(getSession(params.id));
+  }, [dispatch, params]);
 
-    useEffect(() => {
-        dispatch(getSession(params.id));
-    }, [dispatch, params]);
+  useEffect(() => {
+    dispatch(getAllDevices(null, null, "False"));
+  }, [dispatch]);
 
-    useEffect(() => {
-        dispatch(getAllDevices(null, null, 'True'));
-    }, [dispatch]);
-
-    return (
-        <>
-            <div className={styles.back}>
-                <BackButton />
-            </div>
-            <div className={styles.RoomsFormContainer}>
-                {error &&
-                    (Array.isArray(error) ? (
-                        error.map((e) => <Error>{e.msg}</Error>)
-                    ) : (
-                        <Error>{error}</Error>
-                    ))}
-                {success && (
-                    <Success>
-                        {isNew
-                            ? 'Session created successfully'
-                            : 'Session updated successfully'}
-                    </Success>
-                )}
-                <Form
-                    initialState={initialState}
-                    submitHandler={submitHandler}
-                    isNew={isNew}
-                    formName='Session'
-                    button={isNew ? 'Start' : 'Update'}
-                >
-                    {isNew && (
-                        <Select
-                            label='Device'
-                            name='device'
-                            options={devices?.map((r) => r.title) || []} //not active Devices
-                            mult={false}
-                        />
-                    )}
-                    <Select
-                        label='Type'
-                        name='type'
-                        options={['Single', 'Multi', 'Match']}
-                        mult={false}
-                    />
-                </Form>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className={styles.back}>
+        <BackButton />
+      </div>
+      <div className={styles.RoomsFormContainer}>
+        {error &&
+          (Array.isArray(error) ? (
+            error.map((e) => <Error>{e.msg}</Error>)
+          ) : (
+            <Error>{error}</Error>
+          ))}
+        {success && (
+          <Success>
+            {isNew
+              ? "Session created successfully"
+              : "Session updated successfully"}
+          </Success>
+        )}
+        <Form
+          initialState={initialState}
+          submitHandler={submitHandler}
+          isNew={isNew}
+          formName="Session"
+          button={isNew ? "Start" : "Update"}
+        >
+          {isNew && (
+            <Select
+              label="Device"
+              name="device"
+              options={devices?.map((r) => r.title) || []} //not active Devices
+              mult={false}
+            />
+          )}
+          <Select
+            label="Type"
+            name="type"
+            options={["Single", "Multi", "Match"]}
+            mult={false}
+          />
+        </Form>
+      </div>
+    </>
+  );
 };
 
 export default SessionsForm;
